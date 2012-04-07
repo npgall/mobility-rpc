9c6424d11b9a0ef62501f44f1b13980692ce99fb
niall@npgall.com <niall@npgall.com@89c488de-2e69-529f-1a03-d46ea577c354>
2012-04-07 00:50:25 +0000

    [maven-release-plugin]  copy for tag 0.5.4

---
+++ mobility-rpc/tags/0.5.4/src/main/java/com/googlecode/mobilityrpc/network/impl/ConnectionStateListener.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2011 Niall Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.mobilityrpc.network.impl;
+
+import com.googlecode.mobilityrpc.network.ConnectionId;
+
+/**
+ * An internal interface used by the framework - implemented by an object which can be notified when connections are
+ * opened or closed.
+ * <p/>
+ * Note: in practice this is implemented by the {@link com.googlecode.mobilityrpc.network.ConnectionManager} as a means
+ * to be notified of connection events, and this interface is invoked by {@link ConnectionListener}s when incoming
+ * connections are received and by {@link com.googlecode.mobilityrpc.network.Connection} objects when connections are
+ * closed.
+ * <p/>
+ * This interface really only exists to decouple objects which send notifications from the implementations which
+ * subscribe to them, to simplify independent unit testing etc.
+ *
+ * @author Niall Gallagher
+ */
+public interface ConnectionStateListener {
+
+    /**
+     * Called when the supplied connection has been opened.
+     * @param connection The connection which has been opened
+     */
+    public void notifyConnectionOpened(ConnectionInternal connection);
+
+    /**
+     * Called when the supplied connection has been closed.
+     * @param connection The connection which has been closed
+     */
+    public void notifyConnectionClosed(ConnectionInternal connection);
+
+    /**
+     * @param connectionId The connection id to check
+     * @return Allows the sender of notifications to query the destination
+     * to see if a specific connection is registered (typically used to check if primary connection is up)
+     */
+    public boolean isConnectionRegistered(ConnectionId connectionId);
+}
---
+++ mobility-rpc/tags/0.5.4/src/main/java/com/googlecode/mobilityrpc/quickstart/StandaloneMobilityServer.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2011 Niall Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.mobilityrpc.quickstart;
+
+import com.googlecode.mobilityrpc.quickstart.util.LoggingUtil;
+
+import java.util.logging.*;
+
+/**
+ * Starts EmbeddedMobilityServer as a standalone application, listening on port 5739 on all network interfaces by default.
+ * <p/>
+ * Supply system property "-Ddebug=true" on the command line to enable debug logging.
+ *
+ * @author Niall Gallagher
+ */
+public class StandaloneMobilityServer {
+
+    /**
+     * @param args Not used
+     */
+    public static void main(String[] args) {
+        // Set up JDK logging...
+        LoggingUtil.setLibraryLoggingLevel(Boolean.getBoolean("debug") ? Level.FINER : Level.INFO);
+        LoggingUtil.setSingleLineLoggingFormat();
+
+        // Start the server...
+        EmbeddedMobilityServer.start();
+        System.out.println("Mobility-RPC Server started, listening on port " + EmbeddedMobilityServer.DEFAULT_PORT + " on the following addresses:");
+        for (String networkAddress : EmbeddedMobilityServer.getAddresses()) {
+            System.out.println(networkAddress);
+        }
+        System.out.println();
+
+        // Register a shutdown hook to stop the server gracefully when JVM is shutting down...
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                EmbeddedMobilityServer.stop();
+                System.out.println("\nMobility-RPC Server stopped");
+            }
+        });
+    }
+
+}
