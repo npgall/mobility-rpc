51c8c53665cf530b76117f7c5750da2f3421053f
niall <niall@npgall.com>
2013-06-20 21:56:31 +0000

    [maven-release-plugin]  copy for tag 1.1.0

---
+++ mobility-rpc/tags/1.1.0/src/main/java/com/googlecode/mobilityrpc/protocol/pojo/ExecutionResponse.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright 2011, 2012 Niall Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.mobilityrpc.protocol.pojo;
+
+/**
+ * @author Niall Gallagher
+ */
+public class ExecutionResponse {
+
+    public enum ExecutionOutcome {
+        VOID_RETURNED,
+        VALUE_RETURNED,
+        FAILURE
+    }
+
+    private final ExecutionOutcome executionOutcome;
+    private final byte[] serializedReturnObject;
+    private final SerializationFormat serializationFormat;
+    private final RequestIdentifier requestIdentifier;
+
+    public ExecutionResponse(ExecutionOutcome executionOutcome, byte[] serializedReturnObject, SerializationFormat serializationFormat, RequestIdentifier requestIdentifier) {
+        this.executionOutcome = executionOutcome;
+        this.serializedReturnObject = serializedReturnObject;
+        this.serializationFormat = serializationFormat;
+        this.requestIdentifier = requestIdentifier;
+    }
+
+    public ExecutionOutcome getExecutionOutcome() {
+        return executionOutcome;
+    }
+
+    public byte[] getSerializedReturnObject() {
+        return serializedReturnObject;
+    }
+
+    public SerializationFormat getSerializationFormat() {
+        return serializationFormat;
+    }
+
+    public RequestIdentifier getRequestIdentifier() {
+        return requestIdentifier;
+    }
+
+    /**
+     * @throws UnsupportedOperationException always, as this object is not intended to be compared for equality
+     * or used as a key in a hash map.
+     */
+    @SuppressWarnings({"EqualsWhichDoesntCheckParameterClass"})
+    @Override
+    public boolean equals(Object o) {
+        throw new UnsupportedOperationException("Not supported.");
+
+    }
+
+    /**
+     * @throws UnsupportedOperationException always, as this object is not intended to be compared for equality
+     * or used as a key in a hash map.
+     */
+    @Override
+    public int hashCode() {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public String toString() {
+        return "ExecutionResponse{" +
+                "executionOutcome=" + executionOutcome +
+                ", serializedReturnObject=" + serializedReturnObject.length + " bytes" +
+                ", serializationFormat=" + serializationFormat +
+                ", requestIdentifier=" + requestIdentifier +
+                '}';
+    }
+}
---
+++ mobility-rpc/tags/1.1.0/src/test/java/com/googlecode/mobilityrpc/benchmarks/rmi/RmiServer.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2011, 2012 Niall Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.mobilityrpc.benchmarks.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Collection;
+
+/**
+ * @author ngallagher
+ * @since 2011-09-07 14:06
+ */
+public class RmiServer  {
+
+    public static void main(String[] args) {
+        try {
+            RmiHandler rmiServer = new RmiHandlerImpl();
+            UnicastRemoteObject.unexportObject(rmiServer, true);
+            RmiHandler stub = (RmiHandler) UnicastRemoteObject.exportObject(rmiServer, 5738);
+
+            // Bind the remote object's stub in the registry...
+            Registry registry = LocateRegistry.createRegistry(1099);
+            registry.bind("RmiHandler", stub);
+        }
+        catch (Exception e) {
+            throw new IllegalStateException("Failed to start RMI RmiHandler", e);
+        }
+    }
+
+    public interface RmiHandler extends Remote {
+        public <T extends Comparable> Collection<T> processRequest(Collection<T> input) throws RemoteException;
+    }
+
+    public static class RmiHandlerImpl extends UnicastRemoteObject implements RmiHandler {
+
+        public RmiHandlerImpl() throws RemoteException {
+            super();
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T extends Comparable> Collection<T> processRequest(Collection<T> input) throws RemoteException {
+            return ServerBusinessLogic.processRequest(input);
+        }
+    }
+}
